4467c91762ce57daae78debb018d5c07217e082f
Yahia Mansour <yahiaarafat222@gmail.com>
2022-05-08 17:11:51 -0700

    1st messag

---
+++ src/softproj/SoftProj.java
@@ -0,0 +1,23 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softproj;
+
+/**
+ *
+ * @author Yahia Arafat
+ */
+public class SoftProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Tasks().setVisible(true);
+        
+    }
+    
+}
